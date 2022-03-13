73eec3ed5578afe677cf907116f979b81b305b83
Jacekg1993 <jacekg93@gmail.com>
2022-03-13 15:41:42 +0100

    Multiple3or5

---
+++ src/codewars/Multiples3or5.java
@@ -0,0 +1,26 @@
+package codewars;
+
+import java.util.Scanner;
+
+public class Multiples3or5 {
+
+	public static int solution(int number) {
+
+//		Scanner input = new Scanner(System.in);
+//		int number = input.nextInt();
+		
+		int result = 0;
+
+		if (number > 0) {
+			for (int i = 0; i < number; i++) {
+					
+					if (i % 3 == 0 || i % 5 == 0) {
+						
+					result += i;
+				}
+			}
+		}
+		
+		return result;
+	}
+}
--- src/codewars/codewars.java
+++ src/codewars/codewars.java
@@ -4,6 +4,6 @@ public class codewars {
 	
 	public static void main(String[] args) {
 		
+		System.out.println(Multiples3or5.solution(10));
 	}
-
 }
