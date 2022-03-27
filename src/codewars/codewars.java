2fc88eb295d2469569c89cf5ea4d88545319954d
Jacekg1993 <jacekg93@gmail.com>
2022-03-27 09:57:18 +0200

    CharacterCounter

--- src/codewars/codewars.java
+++ src/codewars/codewars.java
@@ -1,5 +1,6 @@
 package codewars;
 
+import seven_kyu.CharacterCounter;
 import seven_kyu.DigitsExplosion;
 import seven_kyu.RegexValidatePinCode;
 
@@ -7,6 +8,6 @@ public class codewars {
 	
 	public static void main(String[] args) {
 		
-		System.out.println(DigitsExplosion.explode("014"));
+		System.out.println(CharacterCounter.validateWord("Abcabc"));
 	}
 }
